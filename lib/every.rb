2b297927ddd6723952cf0f322d4245df5633ea81
mynyml <mynyml@gmail.com>
2009-03-16 09:44:10 -0400

    Allow chaining method calls with #every! + #end

--- lib/every.rb
+++ lib/every.rb
@@ -1,15 +1,22 @@
 class Every
   instance_methods.each { |m| undef_method(m) unless m.match(/^__/) }
-  def initialize(obj)
-    @obj = obj
+  def initialize(obj, chain=false)
+    @obj, @chain = obj, chain
   end
   def method_missing(method, *args)
-    @obj.map {|o| o.__send__(method,*args) }
+    @@enum = @obj.map {|o| o.__send__(method,*args) }
+    @chain ? Every.new(@@enum, true) : @@enum
+  end
+  def end
+    @@enum
   end
 end
 
 module Enumerable
   def every
     Every.new(self)
+  end
+  def every!
+    Every.new(self, true)
   end
 end
