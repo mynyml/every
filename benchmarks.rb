bbbd3dcfd55cbad9f64629b8fc363ce29cf2523d
mynyml <mynyml@gmail.com>
2009-03-22 19:29:32 -0400

    Updated benchmarks.

--- benchmarks.rb
+++ benchmarks.rb
@@ -3,7 +3,7 @@
 require 'pathname'
 require 'benchmark'
 root  =  Pathname(__FILE__).dirname
-require root.join('lib/every.rb')
+require  root.join('lib/every.rb')
  
 unless defined?(:a.to_proc)
   class Symbol
@@ -24,7 +24,9 @@ puts
 puts "Two iterations"
 n = 100_000
 Benchmark.bm(15) do |bm|
-  bm.report('Block:')           { (0..n).map {|i| i.floor }.map {|i| i.next } }
+  bm.report('Block:')           { (0..n).map {|i| i.floor.next } }
+  bm.report('Block2:')          { (0..n).map {|i| i.floor }.map {|i| i.next } }
   bm.report('Symbol#to_proc:')  { (0..n).map(&:floor).map(&:next) }
   bm.report('every:')           { (0..n).every.floor.every.next }
+  bm.report('every (chain):')   { (0..n).every { floor.next } }
 end
