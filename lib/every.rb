674d63f607de47e496686a3b6c07fbe9904e1384
mynyml <mynyml@gmail.com>
2009-03-25 10:11:48 -0400

    Added #select_every and #reject_every methods.

--- lib/every.rb
+++ lib/every.rb
@@ -1,10 +1,10 @@
 class Every
   instance_methods.each { |m| undef_method(m) unless m.match(/^__/) }
-  def initialize(enum)
-    @enum = enum
+  def initialize(enum, method=:map)
+    @enum, @method = enum, method
   end
   def method_missing(method, *args, &block)
-    @enum.map {|o| o.__send__(method, *args, &block) }
+    @enum.send(@method) {|o| o.__send__(method, *args, &block) }
   end
 end
 
@@ -13,5 +13,15 @@ module Enumerable
     block_given? ?
       Every.new(self).instance_eval(&block) :
       Every.new(self)
+  end
+  def select_every(&block)
+    block_given? ?
+      Every.new(self, :select).instance_eval(&block) :
+      Every.new(self, :select)
+  end
+  def reject_every(&block)
+    block_given? ?
+      Every.new(self, :reject).instance_eval(&block) :
+      Every.new(self, :reject)
   end
 end
--- test/test_every.rb
+++ test/test_every.rb
@@ -10,17 +10,35 @@ class EveryTest < Test::Unit::TestCase
       whitelist = %w( __id__ __send__ method_missing )
       Every.instance_methods.to_set.should be(whitelist.to_set)
     end
-    test "passes message onto enumerable's items" do
-      [1.4, 2.4, 3.4].every.floor.should be([1,2,3])
+    context "#every" do
+      test "passes message onto enumerable's items" do
+        [1.4, 2.4, 3.4].every.floor.should be([1,2,3])
+      end
+      test "allows arguments" do
+        %w( axb dxf ).every.gsub(/x/,'y').should be(%w( ayb dyf ))
+      end
+      test "allows blocks" do
+        %w( axb dxf ).every.gsub(/x/) { 'y' }.should be(%w( ayb dyf ))
+      end
+      test "accepts multi-level message passing" do
+        [1.4, 2.4, 3.4].every { floor.next + 2 }.should be([4,5,6])
+      end
     end
-    test "allows arguments" do
-      %w( axb dxf ).every.gsub(/x/,'y').should be(%w( ayb dyf ))
+    context "#select_every" do
+      test "passes message onto enumerable's items" do
+        %w( axb ayb xyz ).select_every.match(/x/).should be(%w( axb xyz ))
+      end
+      test "accepts multi-level message passing" do
+        [1.4, 2.4, 3.4].select_every { floor.next > 2 }.should be([2.4, 3.4])
+      end
     end
-    test "allows blocks" do
-      %w( axb dxf ).every.gsub(/x/) { 'y' }.should be(%w( ayb dyf ))
-    end
-    test "accepts multi-level message passing" do
-      [1.4, 2.4, 3.4].every { floor.next + 2 }.should be([4,5,6])
+    context "#reject_every" do
+      test "passes message onto enumerable's items" do
+        %w( axb ayb xyz ).reject_every.match(/x/).should be(%w( ayb ))
+      end
+      test "accepts multi-level message passing" do
+        [1.4, 2.4, 3.4].reject_every { floor.next > 3 }.should be([1.4, 2.4])
+      end
     end
   end
 end
