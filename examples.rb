6b42fc7c78c2dc4f7e7be8ecd23777c09b14aac7
mynyml <mynyml@gmail.com>
2009-04-02 00:55:35 -0400

    Add output in examples file.

--- examples.rb
+++ examples.rb
@@ -4,14 +4,19 @@ require  root.join('lib/every.rb')
 
 enum = [1.4, 2.4 ,3.4]
 puts enum.every.floor.inspect
+#=> [1, 2, 3]
 
 enum = %w( axb dxf )
 puts enum.every.gsub(/x/) { 'y' }.inspect
+#=> ["ayb", "dyf"]
 
 enum = %w( foo bar )
 enum.every.upcase!
 puts enum.inspect
+#=> ["FOO", "BAR"]
 
 enum = %w( foo bar ) << ''
 puts enum.every.empty?.all?
+#=> false
 puts enum.every.empty?.any?
+#=> true
