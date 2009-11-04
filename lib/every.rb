2bde12f0678802eda16ddbb1e7339e674da65cae
mynyml <mynyml@gmail.com>
2009-11-04 09:14:11 -0500

    Clean up a bit

--- lib/every.rb
+++ lib/every.rb
@@ -1,17 +1,15 @@
 module Enumerable
   class Proxy
     instance_methods.each { |m| undef_method(m) unless m.match(/^__/) }
+
     def initialize(enum, method=:map)
       @enum, @method = enum, method
     end
+
     def method_missing(method, *args, &block)
       @enum.__send__(@method) {|o| o.__send__(method, *args, &block) }
     end
   end
-end
 
-module Enumerable
-  def every
-    Proxy.new(self)
-  end
+  def every() Proxy.new(self) end
 end
